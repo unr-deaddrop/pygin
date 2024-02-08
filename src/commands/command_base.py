46ca73b1c5d4cfb62dc1f60651ff6eddb1fc28cb
Lloyd Gonzales <lgonzalesna@gmail.com>
2024-02-08 12:16:21 -0800

    add reqs, fix tox

--- src/commands/command_base.py
+++ src/commands/command_base.py
@@ -4,22 +4,27 @@ Generic definition of a command, possibly including a reference to a renderer.
 TODO: In reality, this should be a completely separate library in a completely separate
 repo. But we'll export it later as needed.
 """
+
 # TODO: make these pydantic models
 # overkill? yea absolutely, but whatever lol
 
-class RendererBase():
+
+class RendererBase:
     pass
 
-class CommandBase():
+
+class CommandBase:
     # The use of abstract properties, as used by Mythic in stock classes,
     # is also valid for Pydantic - see https://github.com/pydantic/pydantic/discussions/2410
     pass
+
 
 def export_all_commands():
     """
     Return a list of available command classes.
     """
     pass
+
 
 def export_commands_as_json():
     """
