d0fe833c08165c5b2537684d28fbc0147f410e07
lgactna <lgonzalesna@gmail.com>
2024-02-25 05:16:55 +0000

    comment in logging module

--- src/agent_code/log_handler.py
+++ src/agent_code/log_handler.py
@@ -5,9 +5,14 @@ This builds on Python's built-in logging module and extends it to allow providin
 additional DeadDrop-specific information inline with log messages. The resulting
 messages can then be converted directly into DeadDrop's message format.
 
-TODO: For some platforms, it's not *really* feasible to just keep sending one
-full messages for one full log. Making this an independent module should make
+For some platforms, it's not *really* feasible to just keep sending one
+full messages for one full log msg. Making this an independent module should make
 it significantly easier to bundle logs together, since we can redirect all logs
 to some coherent database and regularly check to see if we have enough logs to
 force an export.
+
+There's also the problem of deciding whether or not we should regularly export
+logs or always wait until the server asks to the latest log message not yet
+sent to the server.
 """
+
