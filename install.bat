308dfcb8798924fdac2ad6bf5e83dbc6b6477828
lgactna <lgonzalesna@gmail.com>
2024-03-04 10:54:54 -0800

    Reduce scope of requirements

--- install.bat
+++ install.bat
@@ -5,5 +5,5 @@
 : after which it can be assumed that all relevant metadata files are available
 : (and future metadata files, if any) and can be used to generate the relevant
 : models for the Django backend.
-pip install -r requirements.txt -U
+pip install -r install-requirements.txt
 python -m src.meta.generate_metadata
