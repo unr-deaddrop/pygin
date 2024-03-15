dd09e092b941d7f537ee157c64012e0ceeab9207
lgactna <lgonzalesna@gmail.com>
2024-03-14 21:29:56 -0700

    Linting

--- src/meta/exec_message.py
+++ src/meta/exec_message.py
@@ -19,6 +19,7 @@ object.
 
 from pathlib import Path
 from typing import Any
+import json
 import logging
 import sys
 
@@ -26,6 +27,9 @@ from deaddrop_meta.interface_lib import MessagingObject
 from deaddrop_meta.protocol_lib import DeadDropMessage
 from src.agent_code import message_dispatch
 from src.agent_code.config import PyginConfig
+
+from pydantic import TypeAdapter
+import redis
 
 logging.basicConfig(
     handlers=[
@@ -52,34 +56,68 @@ def handle_exception(exc_type, exc_value, exc_traceback):
 sys.excepthook = handle_exception
 
 LOG_FILE_NAME = "message-logs.txt"
+MESSAGE_CONFIG_NAME = "message_config.json"
 PROTOCOL_STATE_NAME = "protocol_state.json"
+MESSAGE_INPUT_NAME = "message.json"
 MESSAGE_OUTPUT_NAME = "messages.json"
 
 
-def translate_config(msg_cfg: MessagingObject) -> dict[str, Any]:
+def select_protocol(msg_cfg: MessagingObject, cfg_obj: PyginConfig) -> str:
+    if msg_cfg.server_config.action == "send":
+        return cfg_obj.INCOMING_PROTOCOLS[0]
+    elif msg_cfg.server_config.action == "receive":
+        return cfg_obj.SENDING_PROTOCOL
+
+    raise RuntimeError(f"Unsupported action {msg_cfg.server_config.action}")
+
+
+def translate_config(msg_cfg: MessagingObject) -> PyginConfig:
+    """
+    Create a new PyginConfig object suitable for passing into the message
+    dispatch unit *as if* it were built for the server.
+    """
     # Construct PyginConfig
+    cfg_obj = PyginConfig(
+        protocol_configuration=msg_cfg.protocol_config, **msg_cfg.agent_config
+    )
 
-    # Select desired protocol
+    # Select desired protocol. In our case, we completely ignore the preferred
+    # protocol set in msg_cfg, because Pygin is only configured to listen/send
+    # over a fixed set of protocols.
+    #
+    # When sending messages, the first configured protocol used for receiving
+    # is selected. When receiving messages, the protocol specified in
+    # SENDING_PROTOCOL is used.
+    #
+    # FIXME: Note that this means that heartbeats and logs, if different
+    # from SENDING_PROTOCOL, will be lost! This will need to be resolved later.
+    # In an ideal world, they'd both accept lists, where the order signifies
+    # priority; if a message fails to send on one protocol, you go to the next.
+    # Exhaust all the protocols, and that's a failure. However, this is logic
+    # more complex than what we're prepared to handle right now, so I'm going
+    # to make this assumption and run with it.
+    protocol_name = select_protocol(msg_cfg, cfg_obj)
 
-    # Get PyginConfig to ProtocolConfig to dict translator
+    if not protocol_name:
+        raise RuntimeError("Could not resolve a protocol name, was one configured?")
 
-    # Invoke translator, returns dict suitable as input to the function as-is
-    raise NotImplementedError
+    proto_cfg = cfg_obj.protocol_configuration[protocol_name].convert_to_server_config(
+        msg_cfg.endpoint_model_data
+    )
+    cfg_obj.protocol_configuration[protocol_name] = proto_cfg
+    return cfg_obj
 
 
 def send_message(msg_cfg: MessagingObject, msg: DeadDropMessage) -> dict[str, Any]:
     """
     Send a message through the command dispatch unit.
     """
-    # Invoke translator, returns dict suitable as input to the function as-is
+    # Invoke translator, returns protocol config
+    cfg_obj = translate_config(msg_cfg)
 
-    # Invoke message dispatch unit (should we just use the already available
-    # Redis service that the server uses? we're assuming that Celery is in use,
-    # so it *is* available for message checking if needed)
-
-    # Return whatever the relevant protocol class returns
-
-    raise NotImplementedError
+    # Invoke message dispatch unit, and return whatever it returns
+    protocol_name = select_protocol(msg_cfg, cfg_obj)
+    return message_dispatch.send_message(msg, protocol_name, cfg_obj)
 
 
 def receive_msgs(msg_cfg: MessagingObject) -> list[DeadDropMessage]:
@@ -91,24 +129,47 @@ def receive_msgs(msg_cfg: MessagingObject) -> list[DeadDropMessage]:
     that have already been seen.
     """
     # Invoke translator, returns dict suitable as input to the function as-is
+    cfg_obj = translate_config(msg_cfg)
 
-    # Invoke message dispatch unit (should we just use the already available
-    # Redis service that the server uses? we're assuming that Celery is in use,
-    # so it *is* available for message checking if needed)
+    # Create new Redis connection (should be valid on the Python implementation
+    # of the DeadDrop server, when containerized). Do not use decode_responses=True,
+    # since this assumption is not used internally.
+    redis_con = redis.Redis(host="redis", port=6379, decode_responses=False)
 
-    # Return whatever the relevant protocol class returns
-    raise NotImplementedError
+    # Invoke message dispatch unit, and return whatever it returns
+    protocol_name = select_protocol(msg_cfg, cfg_obj)
+    return message_dispatch.retrieve_new_messages(protocol_name, cfg_obj, redis_con)
 
 
 if __name__ == "__main__":
     # Get message_config.json, convert to MessagingObject
+    if not Path(MESSAGE_CONFIG_NAME).exists():
+        raise RuntimeError(f"Missing {MESSAGE_CONFIG_NAME}!")
 
-    # Get message.json if present, convert to msg
+    with open(MESSAGE_CONFIG_NAME, "rt") as fp:
+        msg_cfg = MessagingObject.model_validate_json(fp.read())
 
     # Select the correct function to invoke
+    if msg_cfg.server_config.action == "send":
+        # Get message and send
+        if not Path(MESSAGE_INPUT_NAME).exists():
+            raise RuntimeError(f"Missing {MESSAGE_INPUT_NAME} to send!")
 
-    # If receiving messages, write out the messages as messages.json
+        with open(MESSAGE_INPUT_NAME, "rt") as fp:
+            msg = DeadDropMessage.model_validate_json(fp.read())
 
-    # Write out the updated protocol state from MessagingObject if needed
-    # TODO: Not currently supported, because nothing uses this
-    raise NotImplementedError
+        send_message(msg_cfg, msg)
+    elif msg_cfg.server_config.action == "receive":
+        # Receive messages and serialize to JSON
+        result = receive_msgs(msg_cfg)
+
+        ta = TypeAdapter(list[DeadDropMessage])
+        with open(MESSAGE_OUTPUT_NAME, "wb+") as fp2:
+            # Note this returns bytes, not str, so we open the file as binary
+            fp2.write(ta.dump_json(result))
+    else:
+        raise RuntimeError(f"Unknown action {msg_cfg.server_config.action}")
+
+    # Write out the updated protocol state from MessagingObject
+    with open(PROTOCOL_STATE_NAME, "wt+") as fp:
+        json.dump(msg_cfg.protocol_state, fp)
