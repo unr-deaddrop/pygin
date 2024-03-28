58cb425fa215916517d7e02c4b4f852ab24fb6c5
lgactna <lgonzalesna@gmail.com>
2024-03-28 14:47:08 -0700

    Better messages

--- src/meta/replace_install.py
+++ src/meta/replace_install.py
@@ -1,7 +1,20 @@
 from pathlib import Path
+import logging
 import re
+import sys
 
 from src.meta.agent import PyginInfo
+
+logging.basicConfig(
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+    ],
+    level=logging.DEBUG,
+    format="%(filename)s:%(lineno)d | %(asctime)s | [%(levelname)s] - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     target = Path("Makefile")
@@ -12,7 +25,9 @@ if __name__ == "__main__":
     with open(target, "rt") as fp:
         data = fp.read()
         
-    new = re.sub(r"docker pull unrdeaddrop/pygin:.*$", "docker pull unrdeaddrop/pygin:"+PyginInfo.get_version(), data)
+    image_name = "docker pull unrdeaddrop/pygin:"+PyginInfo.get_version()
+    logger.info(f"Writing {image_name=} to Makefile")
+    new = re.sub(r"docker pull unrdeaddrop/pygin:(.*)$", "docker pull unrdeaddrop/pygin:"+PyginInfo.get_version(), data)
     
     with open(target, "wt") as fp:
         fp.write(new)
