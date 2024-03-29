e09beefede780c308097b02784ef54fdc810e5f6
lgactna <lgonzalesna@gmail.com>
2024-03-29 13:29:45 -0700

    Remove push.sh, superseded by GA

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Call this to build and push the repository to Docker Hub with the current
-# Pygin version.
-
-VERSION=$(python3 -m src.meta.agent)
-
-docker build -t unrdeaddrop/pygin:${VERSION} .
-docker push unrdeaddrop/pygin:${VERSION}
